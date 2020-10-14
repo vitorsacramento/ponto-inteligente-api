fb6ed6ec3a9d6181c0a09bab5712469914016484
vitorsacramento <vitorsacramento85@gmail.com>
2020-10-14 18:58:27 -0300

    Commit inicial dos arquivos do projeto

---
+++ src/test/java/com/vitorsacramento/pontointeligente/api/PontoInteligenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitorsacramento.pontointeligente.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoInteligenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
